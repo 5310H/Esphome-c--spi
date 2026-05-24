ff86a1d225a373c96770804ca7f5e7d21db5233f
5310H <5310@trahreg.com>
2026-05-23 20:10:05 -0400

    Update transport.c

--- src/transport/transport.c
+++ src/transport/transport.c
@@ -1,28 +1,62 @@
 #include "esphome_transport.h"
-#include <string.h>
 
-// Placeholder transport layer.
-// Real TCP sockets will be added for Linux + ESP32.
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 int esph_transport_connect(const char *host, uint16_t port) {
-    (void)host;
-    (void)port;
-    // TODO: implement TCP connect
-    return -1; // placeholder: no real socket yet
+    int sock = -1;
+    struct addrinfo hints;
+    struct addrinfo *res = NULL;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family   = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    char port_str[16];
+    snprintf(port_str, sizeof(port_str), "%u", port);
+
+    int err = getaddrinfo(host, port_str, &hints, &res);
+    if (err != 0) {
+        fprintf(stderr, "[TRANSPORT] getaddrinfo failed: %s\n", gai_strerror(err));
+        return -1;
+    }
+
+    sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sock < 0) {
+        perror("[TRANSPORT] socket");
+        freeaddrinfo(res);
+        return -1;
+    }
+
+    if (connect(sock, res->ai_addr, res->ai_addrlen) < 0) {
+        perror("[TRANSPORT] connect");
+        close(sock);
+        freeaddrinfo(res);
+        return -1;
+    }
+
+    freeaddrinfo(res);
+    return sock;
 }
 
 int esph_transport_send(int sock, const uint8_t *buf, int len) {
-    (void)sock;
-    (void)buf;
-    (void)len;
-    // TODO: implement send()
-    return -1;
+    int sent = send(sock, buf, len, 0);
+    if (sent < 0) {
+        perror("[TRANSPORT] send");
+    }
+    return sent;
 }
 
 int esph_transport_recv(int sock, uint8_t *buf, int maxlen) {
-    (void)sock;
-    (void)buf;
-    (void)maxlen;
-    // TODO: implement recv()
-    return -1;
+    int r = recv(sock, buf, maxlen, 0);
+    if (r < 0) {
+        perror("[TRANSPORT] recv");
+    }
+    return r;
 }
