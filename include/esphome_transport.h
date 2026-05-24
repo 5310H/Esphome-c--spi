9db1fe8cf84f72ec9dc18437ba46ce921159fbb0
5310H <5310@trahreg.com>
2026-05-23 20:05:20 -0400

    Update esphome_transport.h

--- include/esphome_transport.h
+++ include/esphome_transport.h
@@ -2,14 +2,43 @@
 #define ESPHOME_TRANSPORT_H
 
 #include <stdint.h>
+#include <stddef.h>
 
-// Open a TCP connection to an ESPHome device
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Open a TCP connection to the given host and port.
+ *
+ * @param host  Null-terminated hostname or IP (e.g. "192.168.1.10")
+ * @param port  TCP port (ESPHome default is 6053)
+ * @return socket fd (>=0) on success, <0 on failure
+ */
 int esph_transport_connect(const char *host, uint16_t port);
 
-// Send raw bytes over the socket
+/**
+ * Send data over an established TCP connection.
+ *
+ * @param sock  Socket fd returned by esph_transport_connect()
+ * @param buf   Data buffer
+ * @param len   Number of bytes to send
+ * @return number of bytes sent, <0 on failure
+ */
 int esph_transport_send(int sock, const uint8_t *buf, int len);
 
-// Receive raw bytes from the socket
+/**
+ * Receive data from an established TCP connection.
+ *
+ * @param sock    Socket fd
+ * @param buf     Destination buffer
+ * @param maxlen  Maximum bytes to read
+ * @return number of bytes received, <0 on failure
+ */
 int esph_transport_recv(int sock, uint8_t *buf, int maxlen);
 
+#ifdef __cplusplus
+}
 #endif
+
+#endif // ESPHOME_TRANSPORT_H
